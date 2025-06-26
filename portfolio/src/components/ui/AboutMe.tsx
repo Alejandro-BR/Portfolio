d4dc7611e4cb98a963b62d3f8a650d7822824ff7
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2025-06-26 20:35:00 +0200

    AboutMe finished
    I think

--- portfolio/src/components/ui/AboutMe.tsx
+++ portfolio/src/components/ui/AboutMe.tsx
@@ -7,49 +7,50 @@ const AboutMe = () => {
 
   return (
     <div className={styles.aboutMe}>
-      <div className={styles.profileAndContact}>
-        <div className={styles.profile}>
-          <div className={styles.profileImage}>
-            <img src="/img/myPhoto.webp" alt="Alejandro BR" />
-          </div>
+      <div className={styles.containerPC}>
+        <div className={styles.profileImage}>
+          <img src="/img/myPhoto.webp" alt="Alejandro BR" />
+        </div>
+
+        <div className={styles.rightColumn}>
           <div className={styles.profileText}>
             <h1 className={styles.name}>Alejandro</h1>
             <h1 className={styles.name}>Barrionuevo Rosado</h1>
             <p className={styles.info}>{t("aboutMe")}</p>
           </div>
-        </div>
 
-        <div className={styles.contactSection}>
-          <div className={styles.links}>
-            <a
-              href="https://github.com/Alejandro-BR"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.linkButton}
-            >
-              <img src="/svg/github.svg" alt="GitHub" />
-              GitHub
-            </a>
-            <a
-              href="https://www.linkedin.com/in/alejandro-barrionuevo-rosado/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.linkButton}
-            >
-              <img src="/svg/linkedin.svg" alt="LinkedIn" />
-              LinkedIn
-            </a>
-            <a
-              href="/docs/Alejandro_Barrionuevo_Rosado_CV.pdf"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.linkButton}
-            >
-              <img src="/svg/contact_page.svg" alt="CV" />
-              CV
-            </a>
+          <div className={styles.contactSection}>
+            <div className={styles.links}>
+              <a
+                href="https://github.com/Alejandro-BR"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.linkButton}
+              >
+                <img src="/svg/github.svg" alt="GitHub" />
+                GitHub
+              </a>
+              <a
+                href="https://www.linkedin.com/in/alejandro-barrionuevo-rosado/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.linkButton}
+              >
+                <img src="/svg/linkedin.svg" alt="LinkedIn" />
+                LinkedIn
+              </a>
+              <a
+                href="/docs/Alejandro_Barrionuevo_Rosado_CV.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.linkButton}
+              >
+                <img src="/svg/contact_page.svg" alt="CV" />
+                CV
+              </a>
+            </div>
+            <EmailActions />
           </div>
-          <EmailActions />
         </div>
       </div>
     </div>
